1ba8b349425eed13a919fab658f23492a2083d07
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2023-11-09 15:46:10 +0100

    feat: add mario icon

--- src/components/pages/protectedArea/Mario.js
+++ src/components/pages/protectedArea/Mario.js
@@ -1,9 +1,30 @@
 import marioVideo from "../../../assets/videos/marioVideo.mp4";
+import superMario from "../../../assets/pics/superMario.png";
 
 export default function Mario() {
   return (
     /*inserire una funzion eper cancellare nazabotdallocal storage */
     <div className="flex justify-center items-center h-screen w-screen backMario">
+      <img
+        src={superMario}
+        alt="Super Mario"
+        className="absolute top-5 left-5 w-12 md:w-20"
+      />
+      <img
+        src={superMario}
+        alt="Super Mario"
+        className="absolute top-5 right-5 w-12 md:w-20"
+      />
+      <img
+        src={superMario}
+        alt="Super Mario"
+        className="absolute bottom-5 left-5 w-12 md:w-20"
+      />
+      <img
+        src={superMario}
+        alt="Super Mario"
+        className="absolute bottom-5 right-5 w-12 md:w-20"
+      />
       <video controls muted autoPlay className="h-5/6 w-10/12 sm:w-auto">
         <source src={marioVideo} type="video/mp4" />
       </video>
