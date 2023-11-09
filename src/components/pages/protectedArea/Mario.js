897aaf1ffebd2b413d77dba931134d142574cff7
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2023-11-09 12:17:58 +0100

    add video to Mario page

--- src/components/pages/protectedArea/Mario.js
+++ src/components/pages/protectedArea/Mario.js
@@ -1,6 +1,12 @@
+import marioVideo from "../../../assets/videos/marioVideo.mp4";
+
 export default function Mario() {
   return (
     /*inserire una funzion eper cancellare nazabotdallocal storage */
-    <div>Mario</div>
+    <div className="flex justify-center items-center h-screen w-screen backMario">
+      <video controls muted autoPlay className="h-5/6 w-10/12 sm:w-auto">
+        <source src={marioVideo} type="video/mp4" />
+      </video>
+    </div>
   );
 }
